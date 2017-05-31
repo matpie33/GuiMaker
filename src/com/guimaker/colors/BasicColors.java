05d660307f7ae55b54c4ee8738239a8df780bac1
Mateusz <Spaaw@poczta.fm>
2017-05-31 19:04:47 +0200

    Added very light blue color.

--- src/com/guimaker/colors/BasicColors.java
+++ src/com/guimaker/colors/BasicColors.java
@@ -8,6 +8,7 @@ public class BasicColors {
 	public static final Color DARK_BLUE = new Color(140, 80, 230);
 	public static final Color VERY_BLUE = new Color(0, 0, 102);
 	public static final Color LIGHT_BLUE = new Color(0, 120, 200);
+	public static final Color VERY_LIGHT_BLUE = new Color(140, 196, 237);
 	public static final Color OCEAN_BLUE = new Color(90, 170, 230);
 	public static final Color GREY = new Color(190, 190, 190);
 
