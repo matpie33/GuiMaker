1dcf99bd04e4c4939beb4841492e76dfc5160a20
Mateusz <Spaaw@poczta.fm>
2017-10-22 00:01:12 +0200

    Add equals and hashcode method to hotkey wrapper - to enable proper work in hash map.

--- src/com/guimaker/utilities/HotkeyWrapper.java
+++ src/com/guimaker/utilities/HotkeyWrapper.java
@@ -1,5 +1,7 @@
 package com.guimaker.utilities;
 
+import java.util.Objects;
+
 public class HotkeyWrapper {
 	private int keyEvent, keyMask;
 
@@ -22,6 +24,20 @@ public class HotkeyWrapper {
 
 	public boolean hasKeyModifier() {
 		return keyMask != KeyModifiers.NONE.getKeyMask();
+	}
+
+	@Override
+	public boolean equals (Object o){
+		if (o instanceof HotkeyWrapper == false){
+			return false;
+		}
+		HotkeyWrapper other = (HotkeyWrapper) o;
+		return other.keyEvent == keyEvent && other.keyMask == keyMask;
+	}
+
+	@Override
+	public int hashCode (){
+		return Objects.hash(keyEvent, keyMask);
 	}
 
 }
